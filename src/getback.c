404e300b939eb1a209bf9a5ffb751fe48ec14dea
Samuel Rinnetmaki <samuel@blip.at>
2013-12-16 22:07:46 +0200

    first attempt to simplify...

--- src/getback.c
+++ src/getback.c
@@ -2,102 +2,48 @@
 
 static Window *window;
 static TextLayer *text_layer;
-double latitude_now;
-double longitude_now;
-double latitude_stored;
-double longitude_stored;
-int accuracy;
-int distance;
-int heading;
-double divider = 1000000;
-
-static const uint32_t LAT1_KEY = 1;
-static const uint32_t LON1_KEY = 2;
-static const uint32_t LAT2_KEY = 3;
-static const uint32_t LON2_KEY = 4;
-static const uint32_t HEAD_KEY = 5;
-static const uint32_t DIST_KEY = 6;
-
-void ftoa(char* str, double val, int precision) {
-  //  start with positive/negative
-  if (val < 0) {
-    *(str++) = '-';
-    val = -val;
-  }
-  //  integer value
-  snprintf(str, 12, "%d", (int) val);
-  str += strlen(str);
-  val -= (int) val;
-  //  decimals
-  if ((precision > 0) && (val >= .00001)) {
-    //  add period
-    *(str++) = '.';
-    //  loop through precision
-    for (int i = 0;  i < precision;  i++)
-      if (val > 0) {
-        val *= 10;
-        *(str++) = '0' + (int) (val + ((i == precision - 1) ? .5 : 0));
-        val -= (int) val;
-      } else
-        break;
-  }
-  //  terminate
-  *str = '\0';
-}
-
-void show_current_position(void) {
-  text_layer_set_text(text_layer, "Dist updated!");
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Body text updated");
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Position: %d, %d", (int) latitude_now, (int) longitude_now);
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Heading: %d, Distance: %d", heading, distance);
-}
-
-void store_current_position(void) {
-  latitude_stored = latitude_now;
-  longitude_stored = longitude_now;
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Stored: %d, %d", (int) latitude_stored, (int) longitude_stored);
-}
+char distance[9] = "";
+char heading[9] = "";
+static const uint32_t CMD_KEY = 1;
+static const uint32_t HEAD_KEY = 2;
+static const uint32_t DIST_KEY = 3;
+static const char *set_cmd = "set";
+static const char *clear_cmd = "clear";
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-/*
-  double lat1 = 60.1;
-  double lon1 = 23.1;
-  double lat2 = 50.2;
-  double lon2 = 25.6;
-  int angle = 0;
-  double sine = sin(angle);
-  sine = sin(lat1)*sin(lat2) + cos(lat1)*cos(lat2) * cos(lon2-lon1);
-  double test = acos(sine);
-  char body_text[20];
-  snprintf(body_text, 20, "%d (%d) %d", (int) test, angle, (int) sine);
-  text_layer_set_text(text_layer, body_text);
-*/
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  store_current_position();
+  // Tuplet cmd_tuple = TupletCString(CMD_KEY, set_cmd);
   DictionaryIterator *iter;
   app_message_outbox_begin(&iter);
   if (iter == NULL) {
     return;
   }
-  dict_write_int32(iter, LAT2_KEY, latitude_now * divider);
-  dict_write_int32(iter, LON2_KEY, longitude_now * divider);
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Built message for phone!");
+  dict_write_int32(iter, HEAD_KEY, 8);
+  dict_write_cstring(iter, CMD_KEY, set_cmd);
+  // dict_write_tuplet(iter, &cmd_tuple);
   app_message_outbox_send();
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Send stored position to phone!");
-  // show_current_position();
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Sent command '%s'to phone!", set_cmd);
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  show_current_position();
+  // Tuplet cmd_tuple = TupletCString(CMD_KEY, clear_cmd);
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  if (iter == NULL) {
+    return;
+  }
+  dict_write_cstring(iter, CMD_KEY, clear_cmd);
+  // dict_write_tuplet(iter, &cmd_tuple);
+  app_message_outbox_send();
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Sent command '%s'to phone!", clear_cmd);
 }
 
 void out_sent_handler(DictionaryIterator *sent, void *context) {
    // outgoing message was delivered
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Message accepted by phone!");
 }
-
 
 void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
    // outgoing message failed
@@ -107,19 +53,18 @@ void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, voi
 void in_received_handler(DictionaryIterator *iter, void *context) {
   // incoming message received
   // Check for fields you expect to receive
-  Tuple *lat1_tuple = dict_find(iter, LAT1_KEY);
-  latitude_now = lat1_tuple->value->int32/divider;
-  Tuple *lon1_tuple = dict_find(iter, LON1_KEY);
-  longitude_now = lon1_tuple->value->int32/divider;
   Tuple *head_tuple = dict_find(iter, HEAD_KEY);
   if (head_tuple) {
-    heading = head_tuple->value->int8;
+    strcpy(heading, head_tuple->value->cstring);
   }
   Tuple *dist_tuple = dict_find(iter, DIST_KEY);
   if (dist_tuple) {
-    distance = dist_tuple->value->int8;
+    strcpy(distance, dist_tuple->value->cstring);
   }
-  show_current_position();
+  static char body_text[9];
+  snprintf(body_text, sizeof(body_text), "%s\n%s", heading, distance);
+  text_layer_set_text(text_layer, body_text);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Body text updated");
 }
 
 void in_dropped_handler(AppMessageResult reason, void *context) {
@@ -137,13 +82,11 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_frame(window_layer);
   text_layer = text_layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
-  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD	));
+  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
-  text_layer_set_text(text_layer, "Press a button");
-/*
-  show_current_position();
-*/
+  text_layer_set_text(text_layer, "UP: set\nDOWN: clear.");
+  // text_layer_set_text(text_layer, "HELLO!");
 }
 
 static void window_unload(Window *window) {
@@ -151,14 +94,12 @@ static void window_unload(Window *window) {
 }
 
 static void init(void) {
-  latitude_stored = persist_exists(LAT2_KEY) ? persist_read_int(LAT2_KEY) : 0;
-  longitude_stored = persist_exists(LON2_KEY) ? persist_read_int(LON2_KEY) : 0;
   app_message_register_inbox_received(in_received_handler);
   app_message_register_inbox_dropped(in_dropped_handler);
   app_message_register_outbox_sent(out_sent_handler);
   app_message_register_outbox_failed(out_failed_handler);
   const uint32_t inbound_size = 128;
-  const uint32_t outbound_size = 128;
+  const uint32_t outbound_size = 8;
   app_message_open(inbound_size, outbound_size);
   window = window_create();
   window_set_click_config_provider(window, click_config_provider);
@@ -171,8 +112,6 @@ static void init(void) {
 }
 
 static void deinit(void) {
-  persist_write_int(LAT2_KEY, latitude_stored);
-  persist_write_int(LON2_KEY, longitude_stored);
   window_destroy(window);
 }
 
--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -27,10 +27,13 @@ function locationSuccess(position) {
   console.log("Got location " + position.coords.latitude + ',' + position.coords.longitude);
   lat1 = position.coords.latitude;
   lon1 = position.coords.longitude;
-  var msg = {"lat1": Math.round(lat1*divider),
-             "lon1": Math.round(lon1*divider)};
-  sendMessage(msg);
   calculate();
+}
+
+function storeLocation(position) {
+  lat2 = position.coords.latitude;
+  lon2 = position.coords.longitude;
+  console.log("Stored location " + position.coords.latitude + ',' + position.coords.longitude);
 }
 
 function calculate() {  
@@ -40,8 +43,8 @@ function calculate() {
       console.log("Not moved yet, still at  " + lat1 + ',' + lon1);
       dist = 0;
       head = 0;
-      var msg = {"dist": dist,
-                 "head": head};
+      var msg = {"dist": dist.toString(),
+                 "head": head.toString()};
       sendMessage(msg);
       return;
     }
@@ -63,21 +66,19 @@ function calculate() {
             Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(l1) * Math.cos(l2); 
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
     dist = R * c;    
-    msg["dist"] = dist;
     console.log("Calculated dist " + dist);
     
     var y = Math.sin(dLon) * Math.cos(lat2);
     var x = Math.cos(l1)*Math.sin(l2) -
             Math.sin(l1)*Math.cos(l2)*Math.cos(dLon);
     head = Math.atan2(y, x).toDeg();
-    msg["head"] = head;
     console.log("Calculated head " + head);
-    var msg = {"dist": dist,
-               "head": head};
+    var msg = {"dist": dist.toString(),
+               "head": head.toString()};
     sendMessage(msg);
   }
   else {
-    console.log("no lat2 and lon2: " + lat2 + ',' + lon2);
+    console.log("Will not calculate: no lat2 and lon2: " + lat2 + ',' + lon2);
   }
 }
 
@@ -99,13 +100,25 @@ Pebble.addEventListener("deviceorientation", function(event) {
 
 Pebble.addEventListener("appmessage",
   function(e) {
-    console.log("Received message: " + e.payload);
     // for (var i in e.payload) {
     //   console.log(i + ': ' + e.payload[i]);
     // }
-    if (e.payload["lat2"] || e.payload["lon2"]) {
-      lat2 = e.payload["lat2"]/divider;
-      lon2 = e.payload["lon2"]/divider;
+    if (e.payload["head"]) {
+      console.log("Received command: " + e.payload["head"]);
+      var opts = { "timeout": 15000, "maximumAge": 1000 }; 
+      window.navigator.geolocation.getCurrentPosition(storeLocation, locationError, opts);
+    }
+    else if (e.payload["cmd"] && (e.payload["cmd"] == "clear")) {
+      console.log("Received command: " + e.payload["cmd"]);
+      lat2 = 0;
+      lon2 = 0;
+    }
+    else {
+      var txt = '';
+      for (var i in e.payload) {
+        txt += i + ': ' + e.payload[i] + "\n";
+      }
+      console.log("Received unknown message with " + e.payload.length + " items: " + txt);
     }
     calculate();
   }
